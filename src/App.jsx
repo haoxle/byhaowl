5a32696aff0c76dffca70bdaa86aa7296035d09a
haoxle <hao-le@live.co.uk>
2022-12-02 14:55:57 +0000

    updated routing

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <Routes>
       <Route
-        path="/home"
+        path="/"
         element={
           <>
             <Nav />
--- src/Component/Nav/Nav.jsx
+++ src/Component/Nav/Nav.jsx
@@ -4,7 +4,7 @@ import "./Nav.scss";
 const Nav = () => {
   return (
     <div className="nav-container">
-      <NavLink to={"/home"}>Home</NavLink>
+      <NavLink to={"/"}>Home</NavLink>
       <NavLink to={"/about"}>About</NavLink>
       <NavLink to={"/news"}>News</NavLink>
       <NavLink to={"/shop"}>Shop</NavLink>
