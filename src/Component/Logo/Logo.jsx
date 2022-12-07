7be04518d144acc4bc926eebde871355bb404a5a
haoxle <hao-le@live.co.uk>
2022-12-07 09:58:35 +0000

    added a shopping basket

--- src/Component/Logo/Logo.jsx
+++ src/Component/Logo/Logo.jsx
@@ -1,12 +1,18 @@
 import "./Logo.scss";
 import haoxle from "../../Assets/logo.png";
 import { NavLink } from "react-router-dom";
+import bag from "../../Assets/shopping-bag.png";
 const Logo = () => {
   return (
     <NavLink className="logoNav" to="/">
       <div className="logo-container">
-        <img className="byHaowl-logo" src={haoxle} alt="logo" />
-        {/* <h1 className="byHaowl-title">byHaowl</h1> */}
+        <div className="logo-container__img">
+          <img className="byHaowl-logo" src={haoxle} alt="logo" />
+        </div>
+        <button className="basket">
+          <img className="shopping-basket" src={bag} alt="shopping cart" />
+          <div className="shopping-quantity">1</div>
+        </button>
       </div>
     </NavLink>
   );
