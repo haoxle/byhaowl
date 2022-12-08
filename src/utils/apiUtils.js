05ff09c5ac77a6c4728da8461dbf7ddce1681556
haoxle <hao-le@live.co.uk>
2022-12-08 15:17:57 +0000

    updated styling

--- src/utils/apiUtils.js
+++ src/utils/apiUtils.js
@@ -1,5 +1,5 @@
 export const getCandles = async () => {
-  const response = await fetch("http://localhost:8080/candles");
+  const response = await fetch("http://localhost:8081/candles");
   const data = await response.json();
   return data;
 };
