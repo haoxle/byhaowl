a061e2008cd5491dbb5ed685a08440ffa1cd19cd
haoxle <hao-le@live.co.uk>
2022-12-08 09:30:07 +0000

    commented our add code that did not work

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -5,13 +5,15 @@ export const Cartcontext = createContext();
 export const Context = (props) => {
   const reducer = (state, action) => {
     const flattened = state.flat(1);
-    const valueArr = flattened.map((item) => {
-      return item.id;
-    });
-    const isDuplicate = valueArr.some((item, idx) => {
-      return valueArr.indexOf(item) == idx;
-    });
-    console.log(isDuplicate);
+
+    //this did not work not sure why, this is for ADD
+    // const valueArr = flattened.map((item) => {
+    //   return item.id;
+    // });
+    // const isDuplicate = valueArr.some((item, idx) => {
+    //   return valueArr.indexOf(item) == idx;
+    // });
+    // console.log(isDuplicate);
 
     const increase = flattened.map((item) => {
       if (item.stock === item.quantity) {
